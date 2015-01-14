9728c9e0af19e3b350f960ca4c84902e396d5f61
Roberval Walsh <roberval.walsh@gmail.com>
2015-01-14 19:16:02 +0100

    Die zweite Version der einfach verketteten Liste.

---
+++ Cshp05b10/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cshp05b10
+{
+    // die Klasse für die Listenelement
+    // jetzt auch mit Methoden
+
+    class Listenelement
+    {
+        string daten;
+        Listenelement naechster;
+
+        // die Methode zum Setzen der Daten
+        public void SetDaten(string datenNeu)
+        {
+            // die Zeichenkette setzen
+            daten = datenNeu;
+
+            // das Ende markieren
+            naechster = null;
+        }
+
+        // die Methode zum Anhängen eines neuen Elements
+        // sie ruft sich rekursiv auf, bis das Ende erreicht ist
+        // Zurücklieferung des Endes der Liste
+        public void Anhaengen(string datenNeu)
+        {
+            // wenn das Ende erreicht ist, ein neues Element erzeugen
+            if (naechster == null)
+            {
+                naechster = new Listenelement();
+                naechster.SetDaten(datenNeu);
+            }
+            // sonst ruft sich die Methode selbst wieder auf
+            else
+                naechster.Anhaengen(datenNeu);
+
+            // zur Veranschaulichung der Rekursion
+            Console.WriteLine("Daten {0} wurden eingefügt.", datenNeu);
+
+        }
+
+        // die MEthode zur Ausgabe der Liste
+        // sie ruft sich ebenfalls rekursiv auf, bis das Ende erreicht ist
+        public void Ausgeben()
+        {
+            Console.WriteLine(daten);
+            if (naechster != null)
+                naechster.Ausgeben();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ein neues Listenelement erzeugen
+            Listenelement listenAnfang = new Listenelement();
+
+            // die Daten im ersten Listenelement setzen
+            listenAnfang.SetDaten("Element 1");
+
+            // weitere Elemente in einer Schleife anfügen
+            for (int element = 2; element < 4; element++)
+            {
+                listenAnfang.Anhaengen("Element " + element);
+            }
+
+            // die Liste ausgeben
+            listenAnfang.Ausgeben();
+            
+        }
+    }
+}
