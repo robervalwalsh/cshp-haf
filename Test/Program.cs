c9a362c641f371b35fb63af14771002793267bf9
Roberval Walsh <roberval.walsh@gmail.com>
2015-01-26 21:34:11 +0100

    Erster Commit Einsendeaufgabe 6 Hefte 6

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace Test
 {
+    class Hund
+    {
+        int gewicht;
+         int alter;
+    }
+
+    class KleinerHund : Hund
+    {
+        int groesse;
+        public KleinerHund(int alt)
+        {
+            alt = alter;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            KleinerHund auau = new KleinerHund(10);
+
         }
     }
 }
