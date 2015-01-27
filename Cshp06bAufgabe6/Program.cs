aa62aa2fe431e686796b62e89e3bf3274bf51a32
Roberval Walsh <roberval.walsh@gmail.com>
2015-01-27 18:26:58 +0100

    Korrekte Überschreibung mit override.

--- Cshp06bAufgabe6/Program.cs
+++ Cshp06bAufgabe6/Program.cs
@@ -35,13 +35,60 @@ namespace Cshp06bAufgabe6
         }
 
     }
+
+    // die Klasse Libelle erbt von der Klasse Insekt
+    class Libelle : Insekt
+    {
+        int fluegellaenge;
+
+        // der Konstruktor
+        public Libelle(int laenge, int gewicht, int fluegellaenge) : base (laenge,gewicht)
+        {
+            this.fluegellaenge = fluegellaenge;
+        }
+
+        // die Methode zum Essem
+        public override void Essen()
+        {
+            laenge = laenge + 1;
+            gewicht = gewicht + 1;
+            fluegellaenge = fluegellaenge + 1;
+        }
+
+        // die Methode zur Ausgabe
+        public override void Ausgabe()
+        {
+            Console.WriteLine("Die Libelle ist {0} cm lang,  wiegt {1} Gramm und ihre Fügel sind {2} cm lang.", laenge, gewicht, fluegellaenge);
+        }
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Insekt kleinInsekt = new Insekt(2, 2);
+            // Insekt
+            Console.WriteLine("Insekt");
 
-            kleinInsekt.Ausgabe();
+            // Objekt der Basisklasse erzeugen
+            Insekt insekt = new Insekt(2, 2);
+            // Methoden aufrufen
+            insekt.Ausgabe();
+            insekt.Essen();
+            insekt.Ausgabe();
+
+            // Libelle
+            Console.WriteLine();
+            Console.WriteLine("Libelle");
+
+            // Objekt der Basisklasse erzeugen
+            Libelle libelle = new Libelle(2, 2, 4);
+            // Methoden aufrufen
+            libelle.Ausgabe();
+            libelle.Essen();
+            libelle.Ausgabe();
+
         }
     }
 }
