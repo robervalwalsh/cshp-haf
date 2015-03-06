27a9b63c3daf584bd04baa4cb75e350e6ddbe9aa
Roberval Walsh <roberval.walsh@gmail.com>
2015-03-06 17:43:28 +0100

    Ein einfacher Taschenrechner Hefte7 Lektion4

---
+++ Taschenrechner/Taschenrechner/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Taschenrechner
+{
+    partial class Taschenrechner
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonBerechnen = new System.Windows.Forms.Button();
+            this.buttonBeenden = new System.Windows.Forms.Button();
+            this.labelBeschreibung = new System.Windows.Forms.Label();
+            this.labelAnzeige = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonBerechnen
+            // 
+            this.buttonBerechnen.Location = new System.Drawing.Point(12, 125);
+            this.buttonBerechnen.Name = "buttonBerechnen";
+            this.buttonBerechnen.Size = new System.Drawing.Size(78, 23);
+            this.buttonBerechnen.TabIndex = 0;
+            this.buttonBerechnen.Text = "Berechnen";
+            this.buttonBerechnen.UseVisualStyleBackColor = true;
+            // 
+            // buttonBeenden
+            // 
+            this.buttonBeenden.Location = new System.Drawing.Point(297, 125);
+            this.buttonBeenden.Name = "buttonBeenden";
+            this.buttonBeenden.Size = new System.Drawing.Size(75, 23);
+            this.buttonBeenden.TabIndex = 1;
+            this.buttonBeenden.Text = "Beenden";
+            this.buttonBeenden.UseVisualStyleBackColor = true;
+            // 
+            // labelBeschreibung
+            // 
+            this.labelBeschreibung.AutoSize = true;
+            this.labelBeschreibung.Location = new System.Drawing.Point(12, 109);
+            this.labelBeschreibung.Name = "labelBeschreibung";
+            this.labelBeschreibung.Size = new System.Drawing.Size(54, 13);
+            this.labelBeschreibung.TabIndex = 2;
+            this.labelBeschreibung.Text = "Ergebnis: ";
+            // 
+            // labelAnzeige
+            // 
+            this.labelAnzeige.AutoSize = true;
+            this.labelAnzeige.Location = new System.Drawing.Point(72, 109);
+            this.labelAnzeige.Name = "labelAnzeige";
+            this.labelAnzeige.Size = new System.Drawing.Size(13, 13);
+            this.labelAnzeige.TabIndex = 3;
+            this.labelAnzeige.Text = "0";
+            // 
+            // Taschenrechner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 160);
+            this.Controls.Add(this.labelAnzeige);
+            this.Controls.Add(this.labelBeschreibung);
+            this.Controls.Add(this.buttonBeenden);
+            this.Controls.Add(this.buttonBerechnen);
+            this.Name = "Taschenrechner";
+            this.Text = "Ein einfacher Taschenrechner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBerechnen;
+        private System.Windows.Forms.Button buttonBeenden;
+        private System.Windows.Forms.Label labelBeschreibung;
+        private System.Windows.Forms.Label labelAnzeige;
+    }
+}
+
